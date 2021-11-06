5e43b5fa70ccbc5f2262a02acca2ed04d2c85a56
vadiole <xdmitree@gmail.com>
2021-11-07 01:14:52 +0200

    add theme changing support

--- app/src/main/kotlin/vadiole/template/MainActivity.kt
+++ app/src/main/kotlin/vadiole/template/MainActivity.kt
@@ -1,16 +1,23 @@
 package vadiole.template
 
 import android.app.Activity
+import android.content.res.Configuration
 import android.graphics.Color
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.TextView
 import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
 import androidx.core.view.WindowInsetsCompat.Type.navigationBars
 import androidx.core.view.WindowInsetsCompat.Type.statusBars
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : Activity() {
+
+    private val insetsControllerX: WindowInsetsControllerCompat?
+        get() = WindowCompat.getInsetsController(window, window.decorView)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +37,21 @@ class MainActivity : Activity() {
                 text = "Template by vadiole"
             }
         )
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateSystemBars(newConfig)
+        window.decorView.setBackgroundColor(getColor(R.color.windowBackground))
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        updateSystemBars(resources.configuration)
+    }
+
+    private fun updateSystemBars(configuration: Configuration) {
+        val isLight = SDK_INT >= 30 && configuration.isNightModeActive
+        insetsControllerX?.isAppearanceLightStatusBars = !isLight
+        insetsControllerX?.isAppearanceLightNavigationBars = !isLight
     }
 }
