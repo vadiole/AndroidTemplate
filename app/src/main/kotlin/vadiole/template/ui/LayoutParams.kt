033a5583dad1ff8e2c8c2a6b1873cecab3fbd708
vadiole <xdmitree@gmail.com>
2024-01-25 19:33:39 +0100

    extract layout params

---
+++ app/src/main/kotlin/vadiole/template/ui/LayoutParams.kt
@@ -0,0 +1,6 @@
+package vadiole.template.ui
+
+import android.view.ViewGroup
+
+const val matchParent = ViewGroup.LayoutParams.MATCH_PARENT
+const val wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT
